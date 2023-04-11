c8ba9e391bddd034bc69f3f03cb2fb6f234897e0
Guillaume T <guillaume.turcotte2@ssc-spc.gc.ca>
2023-04-11 16:40:25 -0400

    wip: working index with LLM

--- GenerateLLMIndex/generate.py
+++ GenerateLLMIndex/generate.py
@@ -1,4 +1,5 @@
 import os 
+import requests
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobServiceClient
@@ -10,3 +11,22 @@ client      = SecretClient(vault_url=kv_uri, credential=credential)
 
 storage_connection  = client.get_secret("openai-storage-connection").value
 blob_service_client = BlobServiceClient.from_connection_string(storage_connection)
+
+urls = [
+    "https://plus.ssc-spc.gc.ca/en/topic/who-we-are"
+]
+
+def main():
+    # use beautiful soup to get more urls from the base urls (SSC Plus, Sharepoint, etc.) .. and build a list of stuff to fetch..
+    # todo
+
+    # request text content from each urls ...
+    for url in urls:
+        r = requests.get(url, verify=False)
+        d = r.text
+        print(d)
+
+
+
+if __name__ == "__main__":
+    main()
--- OpenAIRequest/__init__.py
+++ OpenAIRequest/__init__.py
@@ -6,6 +6,7 @@ import io
 
 import azure.functions as func
 import openai
+
 from langchain.llms import AzureOpenAI
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
@@ -45,84 +46,23 @@ azure_openai_uri    = f"https://{openai_endpoint_name}.openai.azure.com"
 credential  = DefaultAzureCredential()
 client      = SecretClient(vault_url=kv_uri, credential=credential)
 
-storage_connection  = client.get_secret("openai-storage-connection").value
+blob_service_client = BlobServiceClient.from_connection_string(client.get_secret("openai-storage-connection").value)
 
-blob_service_client = BlobServiceClient.from_connection_string(storage_connection)
-
-openai.api_key      = client.get_secret("AzureOpenAIKey").value
-openai.api_base     = azure_openai_uri
 openai.api_type     = 'azure'
+openai.api_base     = azure_openai_uri
+openai.api_key      = client.get_secret("AzureOpenAIKey").value
 openai.api_version  = '2022-12-01' # this may change in the future
+
 os.environ["OPENAI_API_TYPE"]   = "azure"
 os.environ["OPENAI_API_BASE"]   = azure_openai_uri
 os.environ["OPENAI_API_KEY"]    = client.get_secret("AzureOpenAIKey").value
 os.environ["OPENAI_API_VERSION"] = '2022-12-01' # this may change in the future
 
-PDFReader = download_loader("PDFReader")
-loader = PDFReader()
+OpendalAzblobReader  = download_loader("OpendalAzblobReader")
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-
-    logging.info('Python HTTP trigger function processed a request.')
-
-    container_client = blob_service_client.get_container_client(container="unstructureddocs")
-
-    logging.debug("Downloading blobs to create index...")
-    # TODO: terrible way to do things, index should be generated elsewhere and simply loaded here.
-    documents = []
-    for blob in container_client.list_blobs():
-        download_blob_to_file(blob_service_client, container_name="unstructureddocs", blob_name=blob.name)
-        documents = loader.load_data(file=Path(blob.name))
-    
-    logging.info("Creating index...")
-
-    # Define prompt helper
-    max_input_size = 3000
-    num_output = 256
-    chunk_size_limit = 1000 # token window size per document
-    max_chunk_overlap = 20 # overlap for each token fragment
-    prompt_helper = PromptHelper(max_input_size=max_input_size, num_output=num_output, max_chunk_overlap=max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
-    llm = NewAzureOpenAI(deployment_name=deployment_name)
-    print(llm)
-    llm_predictor = LLMPredictor(llm=llm)
-    embedding_llm = LangchainEmbedding(OpenAIEmbeddings(model="text-embedding-ada-002", chunk_size=1))
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm)
-    #service_context = ServiceContext(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm, node_parser=None, llama_logger=None)
-
-    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
-    #downloader = blob.download_blob(encoding='UTF-8')
-    #blob_text = downloader.readall()
-    #print(f"Blob contents: {blob_text}")
-
-    # Send a completion call to generate an answer
-    #print('Sending a test completion job')
-    #start_phrase = 'Write a tagline for an ice cream shop. '
-    #response = openai.Completion.create(engine=deployment_name, prompt=start_phrase, max_tokens=10)
-    #text = response['choices'][0]['text'].replace('\n', '').replace(' .', '.').strip()
-    #print(start_phrase+text)
-
-    enable_llm = bool(req.params.get('llm'))
-    msg = req.params.get('msg')
-    if not msg:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            msg = req_body.get('msg')
-
-    answer = index.query(msg)
-
-    if msg:
-        return func.HttpResponse(f"{msg}\nAnwnser:{answer}")
-    else:
-        # ideally return json..
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-
+##############################################################################
+##########################-UTIL FUNCTIONS-####################################
+##############################################################################
 def list_blobs_hierarchical(self, container_client, prefix):
     for blob in container_client.walk_blobs(name_starts_with=prefix, delimiter='/'):
         if isinstance(blob, prefix):
@@ -149,7 +89,6 @@ def download_blob_to_file(blob_service_client: BlobServiceClient, container_name
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 
     basepath = os.path.dirname(blob_name)
-    logging.info("THE BASEPATH IS" + basepath)
     isExist = os.path.exists(os.path.dirname(blob_name))
     if not isExist:
         os.makedirs(os.path.dirname(blob_name))
@@ -157,3 +96,65 @@ def download_blob_to_file(blob_service_client: BlobServiceClient, container_name
     with open(file=blob_name, mode="wb") as sample_blob:
         download_stream = blob_client.download_blob()
         sample_blob.write(download_stream.readall())
+
+##############################################################################
+##########################       MAIN      ###################################
+##############################################################################
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    #container_client = blob_service_client.get_container_client(container="unstructureddocs")
+
+    # TODO: terrible way to do things, index should be generated elsewhere and simply loaded here.
+    #for blob in container_client.list_blobs():
+    #    #uncomment for live download ... test only atm
+    #    #download_blob_to_file(blob_service_client, container_name="unstructureddocs", blob_name=blob.name)
+    #    #documents.append(docs[0] for docs in loader.load_data(file=Path(blob.name)))
+    #    print(blob.name)
+    
+    loader = OpendalAzblobReader(
+        container='unstructureddocs',
+        path='sscplus',
+        endpoint=client.get_secret("openai-storage-connection").value,
+        account_name="scdcciodtoopenaipoc",
+        account_key=client.get_secret("openai-storage-key").value,
+    )
+    documents = loader.load_data()
+    #documents = SimpleDirectoryReader(input_dir='./sscplus').load_data()
+    logging.info("The documents are:" + ''.join(str(x.doc_id) for x in documents))
+
+    logging.info("Creating index...")
+    # Define prompt helper
+    max_input_size = 3000
+    num_output = 256
+    chunk_size_limit = 1000 # token window size per document
+    max_chunk_overlap = 20 # overlap for each token fragment
+    prompt_helper = PromptHelper(max_input_size=max_input_size, num_output=num_output, max_chunk_overlap=max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    # using same dep as model name because of an older bug in langchains lib (now fixed I believe)
+    llm = NewAzureOpenAI(deployment_name=deployment_name, model_name=deployment_name, temperature=0.5)
+    print(llm)
+    llm_predictor = LLMPredictor(llm=llm)
+    embedding_llm = LangchainEmbedding(OpenAIEmbeddings(model="text-embedding-ada-002", chunk_size=1))
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm)
+    #service_context = ServiceContext(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm, node_parser=None, llama_logger=None)
+    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+
+    msg = req.params.get('msg')
+    if not msg:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            msg = req_body.get('msg')
+
+    answer = index.query(msg)
+
+    if msg:
+        return func.HttpResponse(f"{msg}\nAnwnser:{answer}")
+    else:
+        # ideally return json..
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
