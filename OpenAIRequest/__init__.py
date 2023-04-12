1776784604b6098aaef79be03917e27177a42965
Guillaume T <guillaume.turcotte2@ssc-spc.gc.ca>
2023-04-12 08:25:33 -0400

    first poc draft working and ready to push in Azure

--- OpenAIRequest/__init__.py
+++ OpenAIRequest/__init__.py
@@ -1,27 +1,18 @@
-from typing import List
-import json
 import logging
 import os
-import io
+from typing import List
 
 import azure.functions as func
 import openai
-
-from langchain.llms import AzureOpenAI
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobServiceClient
-
-from pathlib import Path
-from llama_index import (
-    GPTSimpleVectorIndex, 
-    download_loader, 
-    LLMPredictor, 
-    PromptHelper,
-    ServiceContext
-)
 from langchain.embeddings import OpenAIEmbeddings
-from llama_index import LangchainEmbedding
+from langchain.llms import AzureOpenAI
+from llama_index import (GPTSimpleVectorIndex, LangchainEmbedding,
+                         LLMPredictor, PromptHelper, ServiceContext,
+                         download_loader)
+
 
 class NewAzureOpenAI(AzureOpenAI):
     stop: List[str] = None
@@ -60,31 +51,61 @@ os.environ["OPENAI_API_VERSION"] = '2022-12-01' # this may change in the future
 
 SimpleDirectoryReader  = download_loader("SimpleDirectoryReader")
 
-##############################################################################
-##########################-UTIL FUNCTIONS-####################################
-##############################################################################
-def list_blobs_hierarchical(self, container_client, prefix):
-    for blob in container_client.walk_blobs(name_starts_with=prefix, delimiter='/'):
-        if isinstance(blob, prefix):
-            # Indentation is only added to show nesting in the output
-            print(f"{self.indent * self.depth}{blob.name}")
-            self.depth += 1
-            self.list_blobs_hierarchical(container_client, prefix=blob.name)
-            self.depth -= 1
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    prompt = req.params.get('prompt')
+    if not prompt:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
         else:
-            print(f"{self.indent * self.depth}{blob.name}")
+            prompt = req_body.get('prompt')
+    
+    temp = req_body.get('temp')
+    temperature = float(temp) if temp else 0.7
 
-def download_blob_to_string(blob_service_client: BlobServiceClient, container_name, blob_name):
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    # Define prompt helper
+    max_input_size = 3000
+    num_output = 256
+    chunk_size_limit = 1000 # token window size per document
+    max_chunk_overlap = 20 # overlap for each token fragment
+    prompt_helper = PromptHelper(max_input_size=max_input_size, num_output=num_output, max_chunk_overlap=max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
-    # encoding param is necessary for readall() to return str, otherwise it returns bytes
-    downloader = blob_client.download_blob(max_concurrency=1, encoding='UTF-8')
-    return downloader.readall()
+    index = get_index(prompt_helper, temperature)
+    answer = index.query(prompt)
 
-def download_blob_to_stream(blob_service_client: BlobServiceClient, container_name, blob_name, stream):
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-    blob_client.download_blob().readinto(stream)
+    if prompt:
+        return func.HttpResponse(f"{prompt}\nAnwnser:{answer}")
+    else:
+        # ideally return json..
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
 
+def get_index(helper: PromptHelper, temperature: float) -> "GPTSimpleVectorIndex":
+    logging.info("Creating index...")
+    container_client = blob_service_client.get_container_client(container="unstructureddocs")
+
+    #TODO: terrible way to do things, index should be generated elsewhere and simply loaded here.
+    for blob in container_client.list_blobs():
+        download_blob_to_file(blob_service_client, container_name="unstructureddocs", blob_name=blob.name)
+    
+
+    documents = SimpleDirectoryReader(input_dir='./sscplus').load_data()
+    #logging.info("The documents are:" + ''.join(str(x.doc_id) for x in documents))
+
+    # using same dep as model name because of an older bug in langchains lib (now fixed I believe)
+    llm = NewAzureOpenAI(deployment_name=deployment_name, model_name=deployment_name, temperature=temperature)
+    print(llm)
+    llm_predictor = LLMPredictor(llm=llm)
+    #current limitation with Azure OpenAI, has to be chunk size of 1
+    embedding_llm = LangchainEmbedding(OpenAIEmbeddings(model="text-embedding-ada-002", chunk_size=1))
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=helper, embed_model=embedding_llm)
+
+    return GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+    
 def download_blob_to_file(blob_service_client: BlobServiceClient, container_name, blob_name):
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 
@@ -96,57 +117,3 @@ def download_blob_to_file(blob_service_client: BlobServiceClient, container_name
     with open(file=blob_name, mode="wb") as sample_blob:
         download_stream = blob_client.download_blob()
         sample_blob.write(download_stream.readall())
-
-##############################################################################
-##########################       MAIN      ###################################
-##############################################################################
-def main(req: func.HttpRequest) -> func.HttpResponse:
-
-    container_client = blob_service_client.get_container_client(container="unstructureddocs")
-
-    # TODO: terrible way to do things, index should be generated elsewhere and simply loaded here.
-    for blob in container_client.list_blobs():
-    #uncomment for live download ... test only atm
-        download_blob_to_file(blob_service_client, container_name="unstructureddocs", blob_name=blob.name)
-        #print(blob.name)
-    
-
-    documents = SimpleDirectoryReader(input_dir='./sscplus').load_data()
-    logging.info("The documents are:" + ''.join(str(x.doc_id) for x in documents))
-
-    logging.info("Creating index...")
-    # Define prompt helper
-    max_input_size = 3000
-    num_output = 256
-    chunk_size_limit = 1000 # token window size per document
-    max_chunk_overlap = 20 # overlap for each token fragment
-    prompt_helper = PromptHelper(max_input_size=max_input_size, num_output=num_output, max_chunk_overlap=max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
-    # using same dep as model name because of an older bug in langchains lib (now fixed I believe)
-    llm = NewAzureOpenAI(deployment_name=deployment_name, model_name=deployment_name, temperature=0.5)
-    print(llm)
-    llm_predictor = LLMPredictor(llm=llm)
-    embedding_llm = LangchainEmbedding(OpenAIEmbeddings(model="text-embedding-ada-002", chunk_size=1))
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm)
-    #service_context = ServiceContext(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm, node_parser=None, llama_logger=None)
-    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
-
-    msg = req.params.get('msg')
-    if not msg:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            msg = req_body.get('msg')
-
-    answer = index.query(msg)
-
-    if msg:
-        return func.HttpResponse(f"{msg}\nAnwnser:{answer}")
-    else:
-        # ideally return json..
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # openai-poc-az-func
 
-poc using the Azure OpenAI cognitive service (chatgpg 3.5 turbo)
+poc using the Azure OpenAI cognitive service (chatgpg 3.5 turbo) and `llama_index` to build and index to use with the LLM
 
-## dev
+## How-to
+
+Pull this project and create your virtual env, install packages and you can simply run `func start` (see [prerequisites](#pre-requisites))
 
 ```bash
 python3 -m venv .venv
@@ -12,7 +14,11 @@ pip install openai azure-identity azure-keyvault-secrets llama-index azure-stora
 
 Then once you finish the setup for configs bellow you can simply `func start` and use your function service.
 
-### keyvault
+### Pre-requisites
+
+* VSCode with Azure Dev tools such as anything needed to run app dev locally.
+* .NET runtime ..
+* etc.
 
 Set KeyVault setting to your `local.settings.json` we will be using `DefaultAzureCredential` to conect to Key Vault.
 
@@ -31,12 +37,13 @@ Set KeyVault setting to your `local.settings.json` we will be using `DefaultAzur
 
 ## Documentation
 
-### LLM index for storing corpo documents to be leveraged
+*  python langchain [documentation](https://python.langchain.com/en/latest/index.html) 
+* [llamahub to get custom loaders](https://llamahub.ai/)
+* [llama_index](https://gpt-index.readthedocs.io/en/latest/index.html) documentation on how to build the index and load it
 
-https://betterprogramming.pub/how-to-build-your-own-custom-chatgpt-with-custom-knowledge-base-4e61ad82427e
+### Other references used as inspiration
 
-#### specific llama index doc
-
+* https://betterprogramming.pub/how-to-build-your-own-custom-chatgpt-with-custom-knowledge-base-4e61ad82427e
 * https://github.com/jerryjliu/llama_index/blob/main/docs/guides/primer/usage_pattern.md
 * https://python.langchain.com/en/harrison-docs-refactor-3-24/modules/models/text_embedding/examples/azureopenai.html
 * https://python.langchain.com/en/harrison-docs-refactor-3-24/modules/models/llms/integrations/azure_openai_example.html
